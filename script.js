43b9ce3da9b9242730bf2dce3217d50c0abdbeb4
Sara Rincón Gonzalez <sararincon@MacBook-Air-de-Sara.local>
2021-10-22 00:03:32 -0300

    WD

---
+++ script.js
@@ -0,0 +1,219 @@
+const radiologiaDatos = [
+  {
+    hora: "11:00",
+    especialista: "IGNACIO SCHULZ",
+    paciente: "FRANCISCA ROJAS",
+    rut: "9878782-1",
+    prevision: "FONASA",
+  },
+  {
+    hora: "11:30",
+    especialista: "FEDERICO SUBERCASEAUX",
+    paciente: "PAMELA ESTRADA",
+    rut: "15345241-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:00",
+    especialista: "FERNANDO WURTHZ",
+    paciente: "ARMANDO LUNA",
+    rut: "16445345-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:30",
+    especialista: "ANA MARIA GODOY",
+    paciente: "MANUEL GODOY",
+    rut: "17666419-0",
+    prevision: "FONASA",
+  },
+  {
+    hora: "16:00",
+    especialista: "PATRICIA SUAZO",
+    paciente: "RAMON ULLOA",
+    rut: "14989389-K",
+    prevision: "FONASA",
+  },
+];
+
+const traumatologiaDatos = [
+  {
+    hora: "8:00",
+    especialista: "MARIA PAZ ALTUZARRA",
+    paciente: "PAULA SANCHEZ",
+    rut: "15554774-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00",
+    especialista: "RAUL ARAYA",
+    paciente: "ANGÉLICA NAVAS",
+    rut: "15444147-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30",
+    especialista: "MARIA ARRIAGADA",
+    paciente: "ANA KLAPP",
+    rut: "17879423-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "ALEJANDRO BADILLA",
+    paciente: "FELIPE MARDONES",
+    rut: "1547423-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "CECILIA BUDNIK",
+    paciente: "DIEGO MARRE",
+    rut: "16554741-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "12:00",
+    especialista: "ARTURO CAVAGNARO",
+    paciente: "CECILIA MENDEZ",
+    rut: "9747535-8",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:30",
+    especialista: "ANDRES KANACRI",
+    paciente: "MARCIAL SUAZO",
+    rut: "11254785-5",
+    prevision: "ISAPRE",
+  },
+];
+
+const dentalDatos = [
+  {
+    hora: "8:30",
+    especialista: "ANDREA ZUÑIGA",
+    paciente: "MARCELA RETAMAL",
+    rut: "11123425-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "MARIA PIA ZAÑARTU",
+    paciente: "ANGEL MUÑOZ",
+    rut: "9878789-2",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "SCARLETT WITTING",
+    paciente: "MARIO KAST",
+    rut: "7998789-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:00",
+    especialista: "FRANCISCO VON TEUBER",
+    paciente: "KARIN FERNANDEZ",
+    rut: "18887662-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:30",
+    especialista: "EDUARDO VIÑUELA",
+    paciente: "HUGO SANCHEZ",
+    rut: "17665461-4",
+    prevision: "FONASA",
+  },
+  {
+    hora: "14:00",
+    especialista: "RAQUEL VILLASECA",
+    paciente: "ANA SEPULVEDA",
+    rut: "14441281-0",
+    prevision: "ISAPRE",
+  },
+];
+//PARTE 1
+let newtrauma = [
+  {
+    hora: "9:00",
+    especialista: "RENÉ POBLETE",
+    paciente: "ANA GELLONA",
+    rut: "13123329-7",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "9:30",
+    especialista: "MARIA SOLAR",
+    paciente: "RAMIRO ANDRADE",
+    rut: "12221451-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00",
+    especialista: "RAUL LOYOLA",
+    paciente: "CARMEN ISLA",
+    rut: "10112348-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30",
+    especialista: "ANTONIO LARENAS",
+    paciente: "PABLO LOAYZA",
+    rut: "13453234-1",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:00",
+    especialista: "MATIAS ARAVENA",
+    paciente: "SUSANA POBLETE",
+    rut: "14345656-6",
+    prevision: "FONASA",
+  },
+];
+
+let concatTrauma = traumatologiaDatos.concat(newtrauma);
+console.log(traumatologiaDatos);
+console.log(concatTrauma);
+
+//PARTE 2
+radiologiaDatos.shift();
+radiologiaDatos.pop();
+console.log(radiologiaDatos);
+
+//PARTE 3
+let dentalString = dentalDatos.map((consulta) => {
+  let val = Object.values(consulta);
+  return "<p>" + val.join(" - ") + "</p>";
+});
+
+console.log(dentalString);
+
+document.write(dentalString.join(""));
+
+//PARTE 4
+
+let allPacients = dentalDatos.concat(newtrauma).concat(radiologiaDatos);
+
+let printPacients = allPacients.map(
+  (atencion) => `<li>${atencion.paciente}</li>`
+);
+
+document.write(printPacients.join(" "));
+
+//PARTE 5
+let filtradoIsapre = dentalDatos
+  .filter((seguro) => seguro.prevision == "ISAPRE")
+  .map((seguro) => `<li>${seguro.paciente} - ${seguro.prevision}</li>`);
+
+document.write("<ul>");
+document.write(filtradoIsapre.join(" "));
+document.write("</ul>");
+
+//PARTE 6
+let filtradoFonasa = traumatologiaDatos
+  .filter((seguro) => seguro.prevision == "FONASA")
+  .map((seguro) => `<li>${seguro.paciente} - ${seguro.prevision}</li>`);
+
+document.write("<ul>");
+document.write(filtradoFonasa.join(" "));
+document.write("</ul>");
